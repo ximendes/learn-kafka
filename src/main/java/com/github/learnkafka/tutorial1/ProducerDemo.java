e32e2b034c7fa84fd149e02a68e0b8d2972055a0
eduardo <eduardo.ximendes@4effect.com.br>
2020-11-26 18:19:10 -0300

    first commit

---
+++ src/main/java/com/github/learnkafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,32 @@
+package com.github.learnkafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+
+    private static final String bootstrapServers = "127.0.0.1:9092";
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+
+    }
+}
