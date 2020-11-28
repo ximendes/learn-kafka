f4e0c16da02a68b3edc182c118966e70b68cc508
Eduardo  Ximendes <ximendes.eduardo@gmail.com>
2020-11-28 20:21:49 -0300

    add gitignore

--- src/main/java/com/github/learnkafka/tutorial2/TwitterProducer.java
+++ src/main/java/com/github/learnkafka/tutorial2/TwitterProducer.java
@@ -1,6 +1,5 @@
 package com.github.learnkafka.tutorial2;
 
-import com.github.learnkafka.tutorial1.producer.ProducerDemoWithCallback;
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
@@ -9,7 +8,6 @@ import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.BasicClient;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.slf4j.Logger;
